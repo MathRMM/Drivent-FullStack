95b19dae15cbdf0e7f9dd66cd58615c70f6ee612
Kássia <kassia_guterres@outlook.com>
2022-12-21 15:30:52 -0300

    style: hotel card style implementation

--- src/pages/Dashboard/Hotel/HotelPage.js
+++ src/pages/Dashboard/Hotel/HotelPage.js
@@ -19,29 +19,55 @@ export default function HotelPage() {
     
     return hotels; 
   };
-  
+
   useEffect(() => {
     if(list) {
       const hotelInfo = listHotels();
       setHotelData(hotelInfo);
     };
   }, []);  
-  
+  //primeira renderizada vem vazia, verificar
   return <> <Subtitle variant='h6'>Primeiro, escolha seu hotel</Subtitle>
     <CardsWrapper> 
-      {hotelData.length !== 0 ? (hotelData.map((hotel) => <Card><h3>{hotel.hotelName}</h3> <img src={hotel.hotelImage} /> </Card> )) : <> Sem hotéis cadastrados no memento</> }
+      {hotelData.length > 0 ? (hotelData.map((hotel) => <Card onClick={(() => alert('teste'))}> <img src={hotel.hotelImage} /> <h6>{hotel.hotelName}</h6> </Card> )) : <> Sem hotéis cadastrados no memento</> } 
     </CardsWrapper>
   </>; 
 };
 
 const CardsWrapper = styled.div`
-  width: auto;
-  background-color: yellow;
+  width: 100%;
+  display: flex;
+  overflow: hidden;
+  overflow-x: auto;
 `;
+
 const Card = styled.div`
   display: flex;
-  flex-wrap: wrap;
-  width: 50px;
-  height: 20px;
-  background-color: aliceblue;
+  flex-direction: column;
+  width: 196px;
+  height: 264px;
+  align-items: center;
+  
+  background-color: #EBEBEB;
+  border-radius: 10px;
+ 
+  margin-right: 20px;
+  padding-left: 14px;
+  padding-right: 14px;
+  cursor: pointer;
+
+  h6 {
+    font-size: 20px;
+    color: #343434;
+  }
+  
+  img {
+    margin: 16px 0 10px 0;
+    width: 168px;
+    height: 109px;
+    object-fit: fill;
+    border-radius: 5px;
+    background-color: pink;
+  }  
+
 `;
