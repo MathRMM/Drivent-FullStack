43adc85407dbc24542d60d034dc11513c4d5a50d
Math <math@pop-os.localdomain>
2023-01-05 17:21:40 -0300

    fix: typing variables

--- src/middlewares/payment-middleware.ts
+++ src/middlewares/payment-middleware.ts
@@ -3,10 +3,10 @@ import { CardPaymentParams } from "@/services/payments-service";
 
 export function creditCardValidation(cardData: CardPaymentParams): string {
   const expiry = cardData.expirationDate.split("/");
-  const isNumberValid = isValid(cardData.number);
-  const isExpiryValid = isExpirationDateValid(expiry[0], expiry[1]);
-  const isCVVValid = isSecurityCodeValid(cardData.number, cardData.cvv);
-  const isIssuerValid = getCreditCardNameByNumber(cardData.number) === cardData.issuer;
+  const isNumberValid: boolean = isValid(cardData.number);
+  const isExpiryValid: boolean = isExpirationDateValid(expiry[0], expiry[1]);
+  const isCVVValid: boolean = isSecurityCodeValid(cardData.number, cardData.cvv);
+  const isIssuerValid: boolean = getCreditCardNameByNumber(cardData.number) === cardData.issuer;
   
   if (!isNumberValid || !isExpiryValid || !isCVVValid || !isIssuerValid) throw { name: "UnauthorizedError", message: "cartao invalido" };
   return "ok";
