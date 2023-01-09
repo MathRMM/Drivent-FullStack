6bec5d5a3b4199fd3c7d98ff3ec3090c22168391
Math <math@pop-os.localdomain>
2023-01-09 16:10:16 -0300

    fix: exchange creditcards.js for card-validator

--- src/middlewares/payment-middleware.ts
+++ src/middlewares/payment-middleware.ts
@@ -5,8 +5,6 @@ export function creditCardValidation(cardData: CardPaymentParams): string {
   const isNumberValid = cardValidator.number(cardData.number);
   const isExpiryValid= cardValidator.expirationDate(cardData.expirationDate);
   const isCVVValid = cardValidator.cvv(cardData.cvv);
-
-  console.log(isNumberValid, isExpiryValid, isCVVValid);
   
   if (!isNumberValid.isValid || !isExpiryValid.isValid || !isCVVValid.isValid) throw { name: "UnauthorizedError", message: "cartao invalido" };
   return "ok";
