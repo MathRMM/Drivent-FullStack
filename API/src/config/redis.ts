696efd71dd434d1135976be63ffc6a17d34905b1
MathRMM <matheusrodrigomm@gmail.com>
2023-02-25 01:43:44 -0300

    feat: docker runner

--- API/src/config/redis.ts
+++ API/src/config/redis.ts
@@ -1,7 +1,13 @@
 import { createClient } from "redis";
 
 async function connectRedis() {
-  const redisClient = createClient();
+  const redisClient = createClient({
+    socket: {
+      host: "redis",
+      port: 6379,
+    },
+    password: process.env.REDIS_PASSWORD,
+  });
   await redisClient.connect();
 
   return redisClient;
--- API/src/utils/redis.ts
+++ API/src/utils/redis.ts
@@ -1,9 +1,10 @@
 import connectRedis from "@/config/redis";
 
 async function saveRedis(obj: Obj) {
+  const redis = await connectRedis();
   try {
     for (const key in obj) {
-      await (await connectRedis()).set(key, JSON.stringify(obj[key]));
+      await redis.SETEX(key, 5, JSON.stringify(obj[key]));
     }
   } catch (error) {
     return error;
@@ -11,14 +12,11 @@ async function saveRedis(obj: Obj) {
 }
 
 async function getRedis(key: string) {
-  const redisJson =  await (await connectRedis()).get(key);
+  const redisJson = await (await connectRedis()).get(key);
   return JSON.parse(redisJson);
 }
 
-export {
-  saveRedis,
-  getRedis
-};
+export { saveRedis, getRedis };
 
 interface Obj {
   [key: string]: string;
